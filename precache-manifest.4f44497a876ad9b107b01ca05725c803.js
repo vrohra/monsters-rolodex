08b9dd3e3b1f0a443f7a6ec8953d316e18464746
Vinod Rohra <vrohra@tripadvisor.com>
2019-12-28 04:40:08 -0500

    Updates

---
+++ precache-manifest.4f44497a876ad9b107b01ca05725c803.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c77579a1da65d90796a258e4c0205364",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e5a89f7c56480da15547",
+    "url": "/monsters-rolodex/static/css/main.80136675.chunk.css"
+  },
+  {
+    "revision": "aaf7bc2e6c6ea494002a",
+    "url": "/monsters-rolodex/static/js/2.c3f6909c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c3f6909c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5a89f7c56480da15547",
+    "url": "/monsters-rolodex/static/js/main.5a0fa7b3.chunk.js"
+  },
+  {
+    "revision": "56fd92ffca0832af862f",
+    "url": "/monsters-rolodex/static/js/runtime-main.77e8d3a0.js"
+  }
+]);
